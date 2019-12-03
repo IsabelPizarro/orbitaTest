93ca8b38776e266c2c60e1c8a58d4713bcfdaa3a
Beatriz <beatriz.delvalle.pachon@bbvaitspain.com>
2019-12-03 14:08:43 +0100

    Add docs

---
+++ docs/precache-manifest.ad8340369d34ac9d69779ba6f4491140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2bc769faaf263a5a5b6e33a0d0b1ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff2be79b9adcacca4a22",
+    "url": "./static/css/main.d32e636e.chunk.css"
+  },
+  {
+    "revision": "cd6c62b84d08d721d513",
+    "url": "./static/js/2.b5eefe14.chunk.js"
+  },
+  {
+    "revision": "ff2be79b9adcacca4a22",
+    "url": "./static/js/main.b3d1baae.chunk.js"
+  },
+  {
+    "revision": "6561ad0144a571921309",
+    "url": "./static/js/runtime~main.b1381f84.js"
+  }
+]);
